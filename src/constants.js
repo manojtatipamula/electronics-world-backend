65b239c255b674990bf7fe809aaa80a95790a64d
manoj <107401848+manojtatipamula@users.noreply.github.com>
2024-06-21 02:41:02 +0530

    functionality complete 1 (#7)

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const constants = Object.freeze({
   // DB_NAME: `integration_ninjas`,
   DB_NAME: 'electronics_world',
   PASSWORD_SALT_ROUNDS: 10,
   JWT_SECRET: 'SUPER_SECRET',
   JWT_EXPIRY: '30d',
-  STRIPE_SUCCESS_URL : 'http://localhost:4200/payment-complete',
-  STRIPE_CANCEL_URL : 'http://localhost:4200/payment-cancel',
+  STRIPE_SUCCESS_URL : `${process.env.UI_HOST}/payment-complete?session_id={CHECKOUT_SESSION_ID}`,
+  STRIPE_CANCEL_URL : `${process.env.UI_HOST}/cart`,
   STRIPE_PAYMENT_MODE: 'payment',
   STRIPE_CURRENCY: 'usd'
 });
--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -5,5 +5,6 @@ const orderController = require('../controllers/orderController');
 orderRoutes.post('/createOrder', middlewares.validateToken, orderController.createOrder);
 orderRoutes.get('/getAllOrders', middlewares.validateToken, orderController.getAllOrders);
 orderRoutes.get('/getOrder/:id', middlewares.validateToken, orderController.getOrderbyID);
+orderRoutes.put('/updateOrder/:id', middlewares.validateToken, orderController.updateOrderbyID);
 
 module.exports = orderRoutes;
